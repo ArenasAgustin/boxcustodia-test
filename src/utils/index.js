b0ea40e77643576915b967b5530a6d35c036c914
“ArenasAgustin” <arenasagustin7@gmail.com>
2022-10-07 16:57:35 -0300

    no console.log

--- src/utils/index.js
+++ src/utils/index.js
@@ -7,20 +7,17 @@ const setPath = (value) => {
 
   pathname = pathname.split("/");
   pathname.shift();
-  console.log(pathname);
+
   if (value && pathname[pathname.length - 1] !== "documents") {
-    console.log("a");
     pathname[pathname.length - 1] = value;
   } else if (value) {
-    console.log("b");
     pathname.push(value);
   } else {
-    console.log("c");
     pathname.pop();
   }
 
   pathname = pathname.join("/");
-  console.log(pathname);
+
   url = new URL(`${origin}/${pathname}`);
 
   window.history.pushState({}, "", url);
